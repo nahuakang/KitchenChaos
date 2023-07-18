c84b8b925d30efd9b3c73148dbffb06ced93e132
Nahua Kang <kangnahua@gmail.com>
2023-07-18 17:16:06 +0200

    13: Clear Counter prefab and layer mask

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,24 +7,64 @@ public class Player : MonoBehaviour
     // The GameObject containing the `GameInput.cs` script for player input systems
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = .7f;
+    [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
+    private const float PlayerRadius = .7f;
+    private const float PlayerHeight = 2f;
+    // Keeps track of the last player direction for interacting with other objects
+    private Vector3 lastInteractDirection;
 
     private void Update()
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+       HandleMovement();
+       HandleInteractions();
+    }
 
-        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        return new Vector3(inputVector.x, 0f, inputVector.y);
+    }
+
+    private void HandleInteractions()
+    {
+        Vector3 moveDirection = GetMoveDirection();
+
+        if (moveDirection != Vector3.zero)
+        {
+            lastInteractDirection = moveDirection;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            // Try get a ClearCounter
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 moveDirection = GetMoveDirection();
 
         float moveDistance = moveSpeed * Time.deltaTime;
-        float playerRadius = .7f;
-        float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight,
+            PlayerRadius, moveDirection, moveDistance);
 
         if (!canMove)
         {
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight,
+                PlayerRadius, moveDirectionX, moveDistance);
 
             if (canMove)
             {
@@ -33,7 +73,8 @@ public class Player : MonoBehaviour
             else
             {
                 Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight,
+                    PlayerRadius, moveDirectionZ, moveDistance);
 
                 if (canMove)
                 {
@@ -41,19 +82,15 @@ public class Player : MonoBehaviour
                 }
             }
         }
+
         if (canMove)
         {
-            transform.position +=  moveDistance * moveDirection;
+            transform.position += moveDistance * moveDirection;
         }
 
         isWalking = moveDirection != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
