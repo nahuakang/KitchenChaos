b366b74d67b06c78e63b1390cf6c9c40725da0c6
Nahua Kang <kangnahua@gmail.com>
2023-07-18 15:11:52 +0200

    12: Collision Detection with Physics.CapsuleCast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     // The GameObject containing the `GameInput.cs` script for player input systems
     [SerializeField] private GameInput gameInput;
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = .7f;
 
     private bool isWalking;
 
@@ -15,7 +15,36 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position +=  Time.deltaTime * moveSpeed * moveDirection;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position +=  moveDistance * moveDirection;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
